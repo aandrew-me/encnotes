b2ca5bbe9164793081e6345b895618ea12619b3e
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-05-30 19:34:28 +0600

    Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# EncNotes
+
+## Encrypted cloud synced Notes.
+
+This is the frontent of EncNotes. All Notes are Encrypted on the client side and sent to the server.
+
+Used Technologies
+- React
+- Tailwind
+- Editor.js
+- PBKDF2 for Password-Based Key Derivation
+- AES-256 for Encrypting Content
+- [Backend](https://github.com/aandrew-me/encnotes-api) Written in Go
+- MongoDB as Database
+- HCaptcha for Captchas
--- src/pages/Rawdata.jsx
+++ src/pages/Rawdata.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 export default function Rawdata() {
 	const url = localStorage.getItem("api-url");
@@ -14,21 +13,19 @@ export default function Rawdata() {
 				},
 			})
 			.then((data) => {
-        console.log(data.data)
+				console.log(data.data);
 				setJsonData(JSON.stringify(data.data, null, "\t"));
 			})
 			.catch((error) => {
 				console.log(error);
-        window.location.href = "/login"
+				window.location.href = "/login";
 			});
 	}, []);
 
 	return (
 		<div>
 			<h1 className="text-center font-semibold">Raw Database Data</h1>
-			<div className="break-words p-2">
-      {jsonData}
-      </div>
+			<div className="break-words p-2">{jsonData}</div>
 		</div>
 	);
 }

1ae425f78571a07d972f2b1d2a2d5b251725a520
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-06-01 14:49:13 +0600

    Style changes for landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-		<div className="bg-bg-primary">
+		<div className="home">
 			<header className="absolute inset-x-0 top-0 z-50">
 				<nav
 					className="flex items-center justify-between p-6 lg:px-8"
@@ -18,7 +18,6 @@ export default function Home() {
 				>
 					<div className="flex lg:flex-1">
 						<a href="#" className="m-1.5 p-1.5">
-							<span className="sr-only">EncNotes</span>
 							<img
 								className="h-10 w-auto"
 								src="/logo.png"
@@ -105,13 +104,25 @@ export default function Home() {
 			</header>
 
 			<div className="relative isolate px-6 pt-14 lg:px-8">
+				<div
+					className="absolute -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+					aria-hidden="true"
+				>
+					<div
+						className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#f35899] to-[#844fe0] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+						style={{
+							clipPath:
+								"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+						}}
+					/>
+				</div>
 				<div className="mx-auto max-w-2xl py-32">
 					<div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
 					<div className="text-center">
 						<h1 className="text-4xl font-bold tracking-tight text-text-primary sm:text-6xl">
 							EncNotes
 						</h1>
-						<p className="mt-6 text-lg leading-8 text-text-secondary">
+						<p className="mt-6 text-lg leading-8 text-text-primary">
 							EncNotes is a secure way to store your Notes in the
 							cloud. Your notes are encrypted, so only you can see
 							them. (Still in development)
@@ -123,12 +134,6 @@ export default function Home() {
 							>
 								Get started
 							</a>
-							{/* <a
-									href="#"
-									className="text-sm font-semibold leading-6 text-text-secondary"
-								>
-									Learn more <span aria-hidden="true">→</span>
-								</a> */}
 						</div>
 					</div>
 				</div>
